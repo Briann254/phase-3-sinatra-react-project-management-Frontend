f620a950893c31d7c6390ec02577780b40f6fbfb
git_bnderitu <bnnderitu01@email.com>
2023-03-06 09:37:24 +0300

    editted toolbar

--- src/components/UI/Toolbar/Toolbar.js
+++ src/components/UI/Toolbar/Toolbar.js
@@ -1,23 +1,52 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Toolbar = props => (
-	<nav className="navbar navbar-expand-lg navbar-light bg-light">
-		<Link className="navbar-brand" to="/">Home</Link>		
-		<div className="collapse navbar-collapse" id="toolbar">
-			<ul className="navbar-nav">
-				<li className="nav-item">
-					<Link className="nav-link" to="/employees">Employees</Link>
-				</li>
-				<li className="nav-item">
-					<Link className="nav-link" to="/projects">Projects</Link>
-				</li>
-				<li className="nav-item">
-					<Link className="nav-link" to="/tasks">Tasks</Link>
-				</li>
-			</ul>
-		</div>
-	</nav>
-)
+const Toolbar = () => (
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <Link className="navbar-brand" to="/">
+      @MyApp 
+    </Link>	
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#toolbar"
+      aria-controls="toolbar"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon"></span>
+    </button>	
+    <div className="collapse navbar-collapse" id="toolbar">
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <Link className="nav-link" to="/employees">
+            Employees
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/tasks">
+            Tasks
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/projects">
+            Projects
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/profile">
+            <i className="fas fa-user"></i> Profile
+          </Link>
+        </li>
+        <li className="nav-item">
+          <button className="btn btn-primary">
+            <i className="fas fa-plus"></i> New Project
+          </button>
+        </li>
+      </ul>
+    </div>
+  </nav>
+);
 
-export default Toolbar
+export default Toolbar;
